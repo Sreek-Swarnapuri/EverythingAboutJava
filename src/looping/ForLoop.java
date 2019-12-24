af4f57756ef988fb387b2def85ec81a5220f665e
Sreekar Swarnapuri <sreekar.swarnapuri@hotmail.com>
2019-12-24 17:55:50 +0530

    Added another class in looping package - ForLoop.java

---
+++ src/looping/ForLoop.java
@@ -0,0 +1,13 @@
+package looping;
+
+public class ForLoop {
+
+    public static void main(String[] args){
+
+        for (int i = 1; i <100; i*=2){
+            System.out.println(i);
+        }
+
+    }
+
+}
