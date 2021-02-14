499a88867069d326cef2b794c89477ce4e8b78e2
Sreekar Swarnapuri <sreekar.swarnapuri@hotmail.com>
2021-02-14 19:51:56 +0530

    MultiThreading Demonstration with ThreadPools i.e. ExecutorService

---
+++ src/main/java/multithreading/ThreadPools.java
@@ -0,0 +1,89 @@
+package multithreading;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPools {
+
+    public static void main(String[] args) {
+
+        //This is a simple task wherein we read a bunch of files and count the number of lines in each and
+        //output them to a subsequent output file.
+        Instant start = Instant.now();
+        String[] inFiles = {"src/main/resources/MultiThreading/f1.txt"
+                , "src/main/resources/MultiThreading/f2.txt"
+                , "src/main/resources/MultiThreading/f3.txt"
+                , "src/main/resources/MultiThreading/f4.txt"
+                , "src/main/resources/MultiThreading/f5.txt"
+                , "src/main/resources/MultiThreading/f6.txt"};
+        String[] outFiles = {"src/main/resources/MultiThreading/f1_out.txt"
+                , "src/main/resources/MultiThreading/f2_out.txt"
+                , "src/main/resources/MultiThreading/f3_out.txt"
+                , "src/main/resources/MultiThreading/f4_out.txt"
+                , "src/main/resources/MultiThreading/f5_out.txt"
+                , "src/main/resources/MultiThreading/f6_out.txt"};
+
+
+        ExecutorService es = Executors.newFixedThreadPool(6);
+
+        for (int i = 0; i < inFiles.length; i++) {
+            AdderWithThreadPools adder = new AdderWithThreadPools(inFiles[i], outFiles[i]);
+            es.submit(adder);
+        }
+
+        try {
+            es.shutdown();
+            if(!es.awaitTermination(5, TimeUnit.SECONDS))
+                es.shutdownNow();
+        } catch (Exception e) {
+            System.out.println(e.getClass().getSimpleName() + ":" + e.getMessage());
+            es.shutdownNow();
+        }
+
+        System.out.println("Elapsed Time(MilliSeconds): " + (Duration.between(start,Instant.now()).toMillis() ));
+
+    }
+
+}
+
+@AllArgsConstructor
+@Getter
+@Setter
+class AdderWithThreadPools implements Runnable {
+
+    private String inFile, outFile;
+
+    public void doAdd() throws IOException {
+        int total = 0;
+        String line = null;
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(inFile))) {
+            while ((line = reader.readLine()) != null)
+                total++;
+        }
+
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(outFile))) {
+            writer.write(String.valueOf(total));
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            doAdd();
+        } catch (IOException e) {
+            System.out.println(e.getClass().getSimpleName() + ":" + e.getMessage());
+        }
+    }
+}
